0c5de5a44fd6c9c7ca3c67e42cc00af484d76890
Cam- <citbresearch2@gmail.com>
2013-05-26 12:49:06 -0400

    Cancel in-progress calls, Download unread voicemails as MP3 files.
    Cancel in-progress calls previously placed using callNumber function.
    Download all unread Google Voice voicemails as indididual MP3 files.

--- example.php
+++ example.php
@@ -8,6 +8,9 @@ $gv = new GoogleVoice('your.email@domain.com', 'password');
 // Call a phone from one of your forwarding phones.
 $gv->callNumber('9995551212', '5558675309', 'mobile');
 
+// Cancel in-progress call placed using callNumber.
+$gv->cancelCall('9995551212', '5558675309', 'mobile');
+
 // Send an SMS to a phone number.
 $gv->sendSMS('9995551212', 'Sending a message!');
 
@@ -28,6 +31,27 @@ $voice_mails = $gv->getUnreadVoicemail();
 $msgIDs = array();
 foreach($voice_mails as $v) {
 	echo 'Message from: '.$v->phoneNumber.' on '.$v->displayStartDateTime.': '.$v->messageText."<br><br>\n";
+	if(!in_array($v->id, $msgIDs)) {
+		// Mark the message as read in your Google Voice Voicemail.
+		$gv->markMessageRead($v->id);
+		$msgIDs[] = $v->id;
+	}
+}
+
+// Download all unread Google Voice voicemails as indididual MP3 files.
+$voice_mails = $gv->getUnreadVoicemail();
+$msgIDs = array();
+foreach($voice_mails as $v) {
+	// Uncomment next line if you want message texts displayed as in previous example.
+	// echo 'Message from: '.$v->phoneNumber.' on '.$v->displayStartDateTime.': '.$v->messageText."<br><br>\n";
+	// Download individual voicemail.
+	$mp3 = $gv->getVoicemailMP3($v->id);
+	// construct mp3 filename using message id as filename.
+	$mp3file = $v->id.".mp3";
+	// write mp3 file to disk.
+	$fh = fopen($mp3file, 'w') or die("can't open file");
+	fwrite($fh, $mp3);
+	fclose($fh);
 	if(!in_array($v->id, $msgIDs)) {
 		// Mark the message as read in your Google Voice Voicemail.
 		$gv->markMessageRead($v->id);
